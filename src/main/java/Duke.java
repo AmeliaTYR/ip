933466003bff680f40184292f46e1a1f6ed32592
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2020-08-29 19:23:52 +0800

    Replace unicode symbols with hex values

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,70 +3,121 @@ import java.util.Scanner;
 import java.util.LinkedList;
 
 public class Duke {
+
+    /**
+     * Version info of the program.
+     */
+    private static final String VERSION = "Tootie - Version 1.1";
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final char INPUT_COMMENT_MARKER = '#';
+
+    public static final String NEWLINE = System.lineSeparator();
+
+    // Tootie logos
+    public static final String THICK_TOOTIE_LOGO = "88888888888                888    d8b          " + NEWLINE
+            + "    888                    " + "888" +
+            "    Y8P          " + NEWLINE + "    888                    888                 " + NEWLINE + "    " +
+            "888   .d88b.   .d88b.  888888 888  .d88b.  " + NEWLINE + "    888  d88\"\"88b d88\"\"88b 888    888 " +
+            "d8P  Y8b " + NEWLINE + "    888  888  888 888  888 888    888 88888888 " + NEWLINE + "    888  Y88." +
+            ".88P Y88..88P Y88b.  888 Y8b.     " + NEWLINE + "    888   \"Y88P\"   \"Y88P\"   \"Y888 888  \"Y8888" +
+            "  " + NEWLINE;
+    public static final String TRAIN_THEME_TOOTIE_LOGO = "  _____                    _        _            " + NEWLINE
+            + " |_   _|   ___     ___    " + "| " +
+            "|_     (_)     ___   " + NEWLINE + "   | |    / _ \\   / _ \\  " + " |" + "  _|    | |   " + " /" +
+            " -_)  " + NEWLINE + "  _|_|_   \\___/   \\___/   _\\__|   _|_|_   \\___|  " + NEWLINE + "_" +
+            "|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_" + "|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| " + NEWLINE +
+            "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' " + NEWLINE;
+
+    public static final String BLOCKY_TOOTIE_LOGO = "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557 "
+            + "\u2588\u2588\u2588\u2588\u2588\u2588" + "\u2557" + "  \u2588\u2588\u2588\u2588\u2588\u2588\u2557 "
+            + "\u2588\u2588\u2588\u2588\u2588" + "\u2588"
+            + "\u2588\u2588\u2557\u2588\u2588\u2557\u2588\u2588\u2588\u2588" + "\u2588\u2588" + "\u2588\u2557"
+            + NEWLINE + "\u255a\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255d"
+            + "\u2588\u2588\u2554\u2550\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550" + "\u2588"
+            + "\u2588\u2557\u255a\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255d\u2588\u2588" + "\u2551\u2588\u2588"
+            + "\u2554\u2550\u2550\u2550\u2550\u255d" + NEWLINE + "   \u2588" + "\u2588\u2551   \u2588\u2588\u2551"
+            + "   \u2588\u2588\u2551\u2588\u2588\u2551   " + "\u2588\u2588\u2551   \u2588\u2588\u2551   "
+            + "\u2588\u2588\u2551\u2588\u2588\u2588\u2588" + "\u2588\u2557  " + NEWLINE + "   \u2588\u2588\u2551 "
+            + "  \u2588\u2588\u2551   " + "\u2588\u2588\u2551\u2588\u2588\u2551   \u2588\u2588\u2551   "
+            + "\u2588\u2588\u2551   " + "\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u255d  " + NEWLINE + " "
+            + "  \u2588" + "\u2588\u2551   \u255a\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\u255a\u2588\u2588"
+            + "\u2588\u2588\u2588\u2588\u2554\u255d   \u2588\u2588\u2551   " + "\u2588\u2588\u2551\u2588"
+            + "\u2588\u2588\u2588\u2588\u2588\u2588\u2557" + NEWLINE + "   \u255a" + "\u2550\u255d  "
+            + "  \u255a\u2550\u2550\u2550\u2550\u2550\u255d  " + "\u255a\u2550\u2550\u2550\u2550\u2550"
+            + "\u255d    \u255a\u2550\u255d   " + "\u255a\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550"
+            + "\u2550\u255d" + NEWLINE;
+    public static final String SIMPLE_TOOTIE_LOGO =
+            " _____           _   _      " + NEWLINE + "|_   _|         | | (_)     " + NEWLINE + "  | | ___  " +
+            " ___ | |_ _  ___ " + NEWLINE + "  | |/ _ \\ / _ \\| __| |/ _ \\" + NEWLINE + "  | | (_) | (_) | |_| " +
+            "|  __/" + NEWLINE + "  \\_/\\___/ \\___/ \\__|_|\\___|" + NEWLINE;
+
+    public static final String TICK_SYMBOL = "[\u2713]";
+    public static final String CROSS_SYMBOL = "[\u2717]";
+
+    // Emoticons in unicode
+    public static final String SPARKLY_EMOTICON =
+            "\u0028\uff89\u25d5\u30ee\u25d5\u0029\uff89\u002a\u003a\uff65\uff9f\u2727";
+    public static final String CONFUSED_EMOTICON = "\u0028\u30fb\u2227\u2010\u0029\u309e";
+    public static final String HAPPY_EMOTICON = "\uff08\u00b4\u30fb\u03c9\u30fb \u0060\uff09";
+    public static final String FLOWER_SMILE_EMOTICON = "(\u25e0\u203f\u25e0\u273f)";
+
+    public static final String SPARKLY_TEXT_DIVIDER = "\u2500\u2500\u2500\u2500\u2500\u2500\u2500 "
+            + "\u2731\u002a\u002e\uff61\u003a\uff61" + "\u2731\u002a\u002e\u003a\uff61\u2727\u002a\u002e\uff61"
+            + "\u2730\u002a\u002e\u003a\uff61\u2727" + "\u002a\u002e\uff61\u003a\uff61\u002a\u002e\uff61\u2731"
+            + " " + "\u2500\u2500\u2500\u2500\u2500" + "\u2500\u2500";
+
+    /**
+     * List of all tasks.
+     */
+    private static LinkedList<Task> allTasks = new LinkedList<Task>();
+
+    /**
+     * Total number of tasks in the list
+     */
+    private static int numListItems = 0;
+
+
     // prints Tootie logo (text art randomized each run)
-    public static void printTootieLogo(){
+    public static void printTootieLogo() {
         String[] logos = new String[4];
-        logos[0] = " _____           _   _      \n" +
-                "|_   _|         | | (_)     \n" +
-                "  | | ___   ___ | |_ _  ___ \n" +
-                "  | |/ _ \\ / _ \\| __| |/ _ \\\n" +
-                "  | | (_) | (_) | |_| |  __/\n" +
-                "  \\_/\\___/ \\___/ \\__|_|\\___|\n";
-        logos[1] = "████████╗ ██████╗  ██████╗ ████████╗██╗███████╗\n" +
-                "╚══██╔══╝██╔═══██╗██╔═══██╗╚══██╔══╝██║██╔════╝\n" +
-                "   ██║   ██║   ██║██║   ██║   ██║   ██║█████╗  \n" +
-                "   ██║   ██║   ██║██║   ██║   ██║   ██║██╔══╝  \n" +
-                "   ██║   ╚██████╔╝╚██████╔╝   ██║   ██║███████╗\n" +
-                "   ╚═╝    ╚═════╝  ╚═════╝    ╚═╝   ╚═╝╚══════╝\n";
-        logos[2] = "  _____                    _        _            \n" +
-                " |_   _|   ___     ___    | |_     (_)     ___   \n" +
-                "   | |    / _ \\   / _ \\   |  _|    | |    / -_)  \n" +
-                "  _|_|_   \\___/   \\___/   _\\__|   _|_|_   \\___|  \n" +
-                "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_" +
-                "|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n" +
-                "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n";
-        logos[3] = "88888888888                888    d8b          \n" +
-                "    888                    888    Y8P          \n" +
-                "    888                    888                 \n" +
-                "    888   .d88b.   .d88b.  888888 888  .d88b.  \n" +
-                "    888  d88\"\"88b d88\"\"88b 888    888 d8P  Y8b \n" +
-                "    888  888  888 888  888 888    888 88888888 \n" +
-                "    888  Y88..88P Y88..88P Y88b.  888 Y8b.     \n" +
-                "    888   \"Y88P\"   \"Y88P\"   \"Y888 888  \"Y8888  \n";
+        logos[0] = SIMPLE_TOOTIE_LOGO;
+        logos[1] = BLOCKY_TOOTIE_LOGO;
+        logos[2] = TRAIN_THEME_TOOTIE_LOGO;
+        logos[3] = THICK_TOOTIE_LOGO;
         Random rand = new Random(System.currentTimeMillis());
-        System.out.println("Hello from\n" + logos[Math.abs(rand.nextInt()%4)]);
+        System.out.println("Hello from" + NEWLINE + logos[Math.abs(rand.nextInt() % 4)] + NEWLINE + VERSION);
 
     }
 
     // prints the line divider
-    public static void printDivider(){
-        System.out.println("─────── ✱*.｡:｡✱*.:｡✧*.｡✰*.:｡✧*.｡:｡*.｡✱ ───────");
+    public static void printDivider() {
+        System.out.println(SPARKLY_TEXT_DIVIDER);
     }
 
     // prints the hello when starting
-    public static void printHelloMessage(){
-        String helloGreeting = "Hello! I'm Tootie!\n" +
-                "What can I do for you?\n";
+    public static void printHelloMessage() {
+        String helloGreeting = "Hello! I'm Tootie!" + NEWLINE + "What can I do for you?" + NEWLINE;
         printDivider();
         System.out.print(helloGreeting);
         printDivider();
     }
 
     // prints farewell message
-    public static void printFarewellMessage(){
-        String farewellGreeting = "Bye! Hope to see you again soon! (◠‿◠✿)\n";
+    public static void printFarewellMessage() {
+        String farewellGreeting = "Bye! Hope to see you again soon! " + FLOWER_SMILE_EMOTICON + NEWLINE;
         System.out.print(farewellGreeting);
         printDivider();
     }
 
     // prints all list items with index and check
-    public static void printListItems(int numListItems, LinkedList<Task> taskList){
+    public static void printListItems(int numListItems, LinkedList<Task> taskList) {
         for (int i = 0; i < numListItems; i++) {
             System.out.print((i + 1) + ". ");
-            if (taskList.get(i).getIsComplete()){
-                System.out.print("[✓]");
+            if (taskList.get(i).isComplete()) {
+                System.out.print(TICK_SYMBOL);
             } else {
-                System.out.print("[✗]");
+                System.out.print(CROSS_SYMBOL);
             }
             System.out.println(taskList.get(i).getTaskName());
         }
@@ -75,8 +126,9 @@ public class Duke {
     }
 
     // TODO: add a help option to print list of commands
-    public static void help(){
-
+    public static void help() {
+        String helpCommand = "";
+        System.out.println("Here is the list of commands I understand!" + NEWLINE);
     }
 
     // updates tasks isComplete to true, returns -1 if userInput invalid
@@ -84,7 +136,7 @@ public class Duke {
         int taskNum;
         try {
             taskNum = Integer.parseInt(userInput.replaceAll("[\\D]", ""));
-            if (taskNum > numListItems){
+            if (taskNum > numListItems || taskNum < 1) {
                 return -1;
             } else {
                 taskList.get(taskNum - 1).setComplete(true);
@@ -96,53 +148,58 @@ public class Duke {
     }
 
     public static void main(String[] args) {
+        // TODO: make get user input its own function
         String userInput;
-        Scanner in = new Scanner(System.in);
-        LinkedList<Task> taskList = new LinkedList<Task>();
-        int numListItems = 0;
+
         int taskNum;
 
         printTootieLogo();
         printHelloMessage();
 
-        userInput = in.nextLine();
+        userInput = SCANNER.nextLine();
 
+        // TODO: only add things to list when add command is used, otherwise ignore
         // process all user inputs
-        while (!userInput.equals("bye")){
-            if (userInput.equals("list")){
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
                 // print items from the list if any
                 printDivider();
-                if (numListItems == 0){
-                    System.out.println("No tasks found! （´・ω・ `）");
+                if (numListItems == 0) {
+                    System.out.println("No tasks found! " + HAPPY_EMOTICON);
                 } else {
-                    printListItems(numListItems, taskList);
+                    printListItems(numListItems, allTasks);
                 }
             } else if (userInput.startsWith("done")) {
                 // update isComplete status of that task
-                taskNum = updateTaskStatus(taskList, userInput, numListItems);
-                if (taskNum == -1){
+                taskNum = updateTaskStatus(allTasks, userInput, numListItems);
+                // TODO: move the printing into the function updateTaskStatus
+                if (taskNum == -1) {
                     printDivider();
-                    System.out.println("No such task? (・∧‐)ゞ");
+                    System.out.println("No such task? " + CONFUSED_EMOTICON);
                 } else {
                     printDivider();
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println("    [✓]" + taskList.get(taskNum - 1).getTaskName());
-                    System.out.println("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧");
+                    System.out.println("    " + TICK_SYMBOL + allTasks.get(taskNum - 1).getTaskName());
+                    System.out.println(SPARKLY_EMOTICON);
                 }
-            } else {
+            } else if (userInput.startsWith("done")){
                 // add new task to list
-                taskList.addLast(new Task(userInput, false));
+                allTasks.addLast(new Task(userInput, false));
                 numListItems++;
                 printDivider();
                 System.out.println("added: " + userInput);
+            } else {
+                System.out.println("Command not found? " + CONFUSED_EMOTICON);
             }
             printDivider();
-            userInput = in.nextLine();
+            userInput = SCANNER.nextLine();
         }
 
         printDivider();
         printFarewellMessage();
     }
 
-
 }
+
+
+
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,6 @@
 public class Task {
     private String taskName;
     private boolean isComplete;
-    // TODO: implement typing for future levels
-//    private String taskType;
 
     public Task(){
         this("no name", false);
@@ -25,27 +23,7 @@ public class Task {
         return taskName;
     }
 
-    public boolean getIsComplete(){
+    public boolean isComplete(){
         return isComplete;
     }
 }
-
-// TODO: in the setting up of the T,D,E variable need to add a value check
-//  in the setter to ensure either T, D or E is set
-
-
-// TODO: implement Deadline
-//public class Deadline extends Task {
-//
-//    protected String by;
-//
-//    public Deadline(String description, String by) {
-//        super(description);
-//        this.by = by;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "[D]" + super.toString() + " (by: " + by + ")";
-//    }
-//}
