60949407589c3b9bca238d4119414ae26755d7f7
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2020-09-30 15:11:26 +0800

    Add more comments

--- src/main/java/duke/constants/TootieConstants.java
+++ src/main/java/duke/constants/TootieConstants.java
@@ -10,11 +10,17 @@ public class TootieConstants {
 
     /** Number of settings saved  */
     public static final int NUMBER_OF_SETTINGS = 4;
-    public static final int ALL_TASKS_FILE_PATH_ARRAY_INDEX = 1;
+
+    /** Return array initial size */
     public static final int NUMBER_OF_FILE_PATHS = 1;
+
+    /** Settings return array indices */
     public static final int SETTINGS_FILE_PATH_ARRAY_INDEX = 0;
+    public static final int ALL_TASKS_FILE_PATH_ARRAY_INDEX = 1;
     public static final int DIVIDER_CHOICE_ARRAY_INDEX = 2;
     public static final int USERNAME_ARRAY_INDEX = 3;
+
+    /** Initial number of tasks in empty list */
     public static final int INITIAL_NUMBER_OF_TASKS_IN_LIST = 0;
     public static final int INITIAL_NUMBER_OF_TASKS_COMPLETED = 0;
 }
